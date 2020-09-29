c931d1920698da0e8dfe47658c8afbc67e01c067
Taftur <linneavik@live.com>
2020-09-29 11:13:46 +0200

    ZombieH

---
+++ Assets/Scripts/HorizontalZombie.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class HorizontalZombie : MonoBehaviour
+{
+    public float speed;
+    private bool movingLeft = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+}
