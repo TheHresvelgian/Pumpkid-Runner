5305ad8716a1750b7b2a085b5815687c0b14a38a
TheHresvelgian <sigrunhog@hotmail.com>
2020-10-01 21:15:23 +0200

    HealthBar

--- Assets/Scripts/SpriteSwap.cs
+++ Assets/Scripts/SpriteSwap.cs
@@ -2,48 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpriteSwap : MonoBehaviour
+public class SpriteSwap : MonoBehaviour
 {
     public Sprite lowHealth; // Displaying sprite for low health.
     public Sprite mediumHealth; // Displaying sprite for medium health.
     public Sprite fullHealth; // Displaying sprite for full health
+    public Sprite noHealth;
     public PlayerDeath playerDeath;
-    private Animator animator;
-    
-    
+
+
 
     private SpriteRenderer spriteRenderer;
+
     private void Start()
     {
         // Grab your sprite renderer from your GameObject.
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
-
     }
 
-    
 
     private void Update ()
     {
-        
+        if (playerDeath.health == 0)
+        {
+            spriteRenderer.sprite = noHealth;
+        }
         if (playerDeath.health == 1)
         {
-            animator.SetInteger("health", 1);
-            
+            spriteRenderer.sprite = lowHealth;
         }
         else if (playerDeath.health == 2)
         {
-            animator.SetInteger("health", 2);
-            
+            spriteRenderer.sprite = mediumHealth;
         }
         else if (playerDeath.health == 3)
         {
-            animator.SetInteger("health", 3);
-
-        }
-        if (playerDeath.health == 0)
-        {
-            animator.SetInteger("health", 0);
+            spriteRenderer.sprite = fullHealth;
         }
     }
 }
