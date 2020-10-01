4351160635211643f6eded7ea3b2346c9add29bc
Taftur <linneavik@live.com>
2020-10-01 20:03:29 +0200

    efb

---
+++ Assets/DetectPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _anim.Play("SpiderDrop");
+        }
+    }
+}
--- Assets/Scripts/WispFire.cs
+++ Assets/Scripts/WispFire.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fire : MonoBehaviour
+public class WispFire : MonoBehaviour
 {
+    // Start is called before the first frame update
     public Transform firePoint;
 
     // Update is called once per frame
