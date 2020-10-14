762d27270ffd7a5320780ea10ca17f648c9fe040
TheHresvelgian <sigrunhog@hotmail.com>
2020-10-14 22:44:04 +0200

    HighScoreCont.

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class HighScore : MonoBehaviour
 {
+    private Transform entryContainer;
+    private Transform entryTemplate;
     private void Awake()
     {
-        
+        entryContainer = transform.Find("highscoreEntryContainer");
+        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -entryTemplate.position.y * i);
+            entryTransform.gameObject.SetActive(true);
+        }
     }
 }
