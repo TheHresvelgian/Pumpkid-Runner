25f398c5b0b197a09aaba95d2adeac840f2e1375
TheHresvelgian <sigrunhog@hotmail.com>
2020-10-15 19:19:35 +0200

    TutorialTest

---
+++ Assets/Scripts/DestroyAfter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Invoke("Destroy", 5f);
+    }
+
+    private void Destroy()
+    {
+        Object.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/DestroyOnCollideScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollideScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("CandyCorn"))
+        {
+
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Lollipop"))
+        {
+
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Schmores"))
+        {
+
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("KvikkLunsj"))
+        {
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Deadly"))
+        {
+
+            Destroy(other.gameObject);
+
+        }
+        else if (other.CompareTag("Heal"))
+        {
+            Destroy(other.gameObject);
+
+        }
+        else if (other.CompareTag("Tutorial"))
+        {
+            Destroy(other.gameObject);
+
+        }
+    }
+}
