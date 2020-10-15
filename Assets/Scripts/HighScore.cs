a584ac9fd2628f4610c79c5e2489f448c39ad441
TheHresvelgian <sigrunhog@hotmail.com>
2020-10-15 09:18:30 +0200

    more high score

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HighScore : MonoBehaviour
 {
     private Transform entryContainer;
     private Transform entryTemplate;
+    
     private void Awake()
     {
         entryContainer = transform.Find("highscoreEntryContainer");
@@ -19,6 +21,25 @@ public class HighScore : MonoBehaviour
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0, -entryTemplate.position.y * i);
             entryTransform.gameObject.SetActive(true);
+
+            int rank = i + 1;
+            string rankString;
+            switch (rank)
+            {
+                default:
+                    rankString = rank + "TH"; break;
+                case 1: rankString = "1ST"; break;
+                case 2: rankString = "2ND"; break;
+                case 3: rankString = "3RD"; break;
+            }
+            entryTransform.Find("Number").GetComponent<Text>().text = rankString;
+            entryTransform.Find("Score").GetComponent<Text>().text = PickUpScript.score.ToString("0"); ;
+            entryTransform.Find("Name").GetComponent<Text>().text = "Name";
         }
+    }
+    private class HighscoreEntry
+    {
+        public int score;
+        public string name;
     }
 }
